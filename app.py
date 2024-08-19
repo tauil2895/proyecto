87f8952bab4528991c510e02d7db29d9c6de2b7a
tauil2895 <tauil2895@gmail.com>
2024-08-19 10:36:56 -0400

    Creacion que ambos graficos

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header()
 
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
+    # es cribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # crear un histograma
@@ -17,12 +16,12 @@ if hist_button: # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(hist, use_container_width=True)
-    
-scattergram = st.button('Construir grafico de dispersion') # crear un botón
+
+scatter_button = st.button('Construir grafico de dispersion') # crear un botón
         
-if scattergram: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+if scatter_button: # al hacer clic en el botón
+    # es cribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # crear un histograma
     scatter = px.scatter(car_data, x="odometer")
